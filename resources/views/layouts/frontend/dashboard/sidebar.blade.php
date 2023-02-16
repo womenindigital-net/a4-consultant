5dde434f02e9d0c3faf200d5d86ec1c360575f2a
Kuhelika-Puja <88089199+Kuhelika-Puja@users.noreply.github.com>
2023-02-16 12:26:18 +0600

    User dashboard design

---
+++ resources/views/layouts/frontend/dashboard/sidebar.blade.php
@@ -0,0 +1,37 @@
+<section class="page-top">
+    <div class="side-bar border_right">
+        
+        <div class="logo design_logo text-center hide_mobile_view ">
+        
+            <a class="d-flex justify-content-center" href="#"><img src="{{asset('assets/image/logo.jpg')}}" alt="Logo"></a>
+            {{-- <a href="#" class="fs-2 text-white rounded-circle" style="border: 1px solid;padding: 2px 7px;"><i class="bi bi-person"></i></a> --}}
+        
+            <h5 style="">Mohammod Nurul Amin</h5>
+            <p>mohammod34@gmail.com</p>
+            <span href="#" class="nav-icon text-end"><i class="bi bi-list"></i></span>
+        </div>
+
+
+        <div class="mobile_menu canvas-menu">
+            <div class="text-center d-md-none mobile_sidebar">
+                <a href="#"><img src="" alt="Logo"></a>
+                <a href="#" class="fs-2 text-white rounded-circle" style="border: 1px solid;padding: 2px 7px;"><i class="bi bi-person"></i></a>
+                <h5 style="">name</h5>
+                <p>email</p>
+            </div>
+            <div class="create_new_btn_sidebar dash_menu mb-2">
+                <a href="#" class="btn"><i class="fa fa-user" aria-hidden="true"></i>User Profile</a>
+            </div>
+            <div class="create_new_btn_sidebar dash_menu mb-2">
+                <a href="#" class="btn"><i class="fa fa-book" aria-hidden="true"></i>Course Info</a>
+            </div>
+            <div class="create_new_btn_sidebar dash_menu mb-2">
+                <a href="#" class="btn"><i class="fa fa-podcast" aria-hidden="true"></i>Live Class Active</a>
+            </div>
+            <div class="create_new_btn_sidebar dash_menu mb-2">
+                <a href="#" class="btn"><i class="fa fa-houzz" aria-hidden="true"></i></i>Help</a>
+            </div>
+        </div>
+    </div>
+</section>
+
